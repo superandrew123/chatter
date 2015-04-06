86eae405c2ba6c1706eb5c6cf6f667f970e35719
Ville Vesterinen <villevesterinen@gmail.com>
2015-04-06 16:54:17 -0400

    add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+
+  validates_presence_of :post_content, :user_id
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
+
+  validates_uniqueness_of :handle, :email
+  validates_presence_of :handle, :email
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+
 end
