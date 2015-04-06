17a060805f200b4fcd64b68d00d8298652001b32
Ville Vesterinen <villevesterinen@gmail.com>
2015-04-06 16:36:19 -0400

    create and destroy test user

--- db/migrate/20150406201632_create_posts.rb
+++ db/migrate/20150406201632_create_posts.rb
@@ -1,6 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.string :post_content
+      t.integer :user_id
 
       t.timestamps null: false
     end
--- db/migrate/20150406201642_create_comments.rb
+++ db/migrate/20150406201642_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-
+      t.string :comment_content
+      t.integer :user_id
+      t.integer :post_id
       t.timestamps null: false
     end
   end
