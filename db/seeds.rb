3eeb160dab5ff6e2dad2651021429f58812f5924
MoPj <wejmps@gmail.com>
2015-04-09 14:41:30 -0400

    pause

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,21 @@ users = User.create([{name: "Andrew Miksch", handle: "superandrew", email: "andr
   {name: "Jada Pinkett Smith", handle: "Jada", email: "jada@email.com", password: "password"},
   {name: "Taylor Swift", handle: "SoSwift", email: "taylor@email.com", password: "password"},
   {name: "Karlie Kloss", handle: "TheKloss", email: "Karlie@email.com", password: "password"},])
-binding.pry
+
 # users.each do |user|
 #  = User.find_by_id(id)
 # and then create it based on that e.g.
+i = 1
+array_ids = User.all.pluck(:id)
+    binding.pry
+  array_ids.each do |user|
+    post.user_id = user
+    post.post_content = "test post content"
+    post.save
+    i +=1
 
-# store.products.create(:product_name => "Product Test", :price => '985.93')
-# #posts = Post.create([{}])
+  end
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
