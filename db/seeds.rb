93f3e5c7eedea37a2818a65fa67af3bfde55bd84
laurahines22 <laurahines22@gmail.com>
2015-04-09 13:15:22 -0400

    seed 10 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+users = User.create([{name: "Andrew Miksch", handle: "superandrew", email: "andrew@email.com", password: "password"}, 
+  {name: "Laura Hines", handle: "laurahines22", email: "laura@email.com", password: "password"},
+  {name: "Maureen Johnson", handle: "MoPj", email: "maureen@email.com", password: "password"},
+  {name: "Ville Vesterinen", handle: "ville", email: "ville@email.com", password: "password"},
+  {name: "Duane Johnson", handle: "theRock", email: "rock@email.com", password: "password"},
+  {name: "Kurt Russell", handle: "KurtyPants", email: "kurt@email.com", password: "password"},
+  {name: "Bruce Springsteen", handle: "theBoss", email: "Boss@email.com", password: "password"},
+  {name: "Jada Pinkett Smith", handle: "Jada", email: "jada@email.com", password: "password"},
+  {name: "Taylor Swift", handle: "SoSwift", email: "taylor@email.com", password: "password"},
+  {name: "Karlie Kloss", handle: "TheKloss", email: "Karlie@email.com", password: "password"},])
+
+#posts = Post.create([{}])
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
