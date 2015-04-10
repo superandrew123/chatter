1f49334ea51e4cb52f96be272948ff4d265dde12
MoPj <wejmps@gmail.com>
2015-04-10 09:14:40 -0400

    countdown version 1 hardcoded expired date

--- app/assets/javascripts/Timer.js
+++ app/assets/javascripts/Timer.js
@@ -1,35 +1,75 @@
-function startTimer(duration, display) {
-    var start = Date.now(),
-        diff,
-        minutes,
-        seconds;
-    function timer() {
-        // get the number of seconds that have elapsed since 
-        // startTimer() was called
-        diff = duration - (((Date.now() - start) / 1000) | 0);
-
-        // does the same job as parseInt truncates the float
-        minutes = (diff / 60) | 0;
-        seconds = (diff % 60) | 0;
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds; 
-
-        if (diff <= 0) {
-            // add one second so that the count down starts at the full duration
-            // example 05:00 not 04:59
-            start = Date.now() + 1000;
+/*
+* Count Down to Post expired Date and Time
+*/
+(function (e) {
+    e.fn.countdown = function (t, n) {
+    function i() {
+        eventDate = Date.parse(r.date) / 1e3;
+        currentDate = Math.floor(e.now() / 1e3);
+        if (eventDate <= currentDate) {
+            n.call(this);
+            clearInterval(interval)
         }
-    };
-    // we don't want to wait a full second before the timer starts
-    timer();
-    setInterval(timer, 1000);
-}
 
-window.onload = function () {
-    var fiveMinutes = 60 * 5,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
+        seconds = eventDate - currentDate;
+        days = Math.floor(seconds / 86400);
+        seconds -= days * 60 * 60 * 24;
+        hours = Math.floor(seconds / 3600);
+        seconds -= hours * 60 * 60;
+        minutes = Math.floor(seconds / 60);
+        seconds -= minutes * 60;
+        days == 1 ? thisEl.find(".timeRefDays").text("day") : thisEl.find(".timeRefDays").text("days");
+        hours == 1 ? thisEl.find(".timeRefHours").text("hour") : thisEl.find(".timeRefHours").text("hours");
+        minutes == 1 ? thisEl.find(".timeRefMinutes").text("minute") : thisEl.find(".timeRefMinutes").text("minutes");
+        seconds == 1 ? thisEl.find(".timeRefSeconds").text("second") : thisEl.find(".timeRefSeconds").text("seconds");
+        if (r["format"] == "on") {
+            days = String(days).length >= 2 ? days : "0" + days;
+            hours = String(hours).length >= 2 ? hours : "0" + hours;
+            minutes = String(minutes).length >= 2 ? minutes : "0" + minutes;
+            seconds = String(seconds).length >= 2 ? seconds : "0" + seconds
+        }
+        if (!isNaN(eventDate)) {
+            thisEl.find(".days").text(days);
+            thisEl.find(".hours").text(hours);
+            thisEl.find(".minutes").text(minutes);
+            thisEl.find(".seconds").text(seconds)
+        } else {
+            alert("Invalid date. Example: 30 Tuesday 2013 15:50:00");
+            clearInterval(interval)
+        }
+    }
+    var thisEl = e(this);
+    var r = {
+        date: null,
+        format: null
+    };
+    t && e.extend(r, t);
+    i();
+    interval = setInterval(i, 1e3)
+    }
+    })(jQuery);
+    $(document).ready(function () {
+    function e() {
+        var e = new Date;
+        e.setDate(e.getDate() + 60);
+        dd = e.getDate();
+        mm = e.getMonth() + 1;
+        y = e.getFullYear();
+        futureFormattedDate = mm + "/" + dd + "/" + y;
+        return futureFormattedDate
+    }
+
+    $("#countdown").countdown({
+        date:  "8 June 2015 09:00:00", //"$("#expiredate").val())",  //$("#expiredate").attr('value'),  //8 June 2015 09:00:00", Change this to your desired date to countdown to
+        format: "on"
+    });
+});
+
+
+// $("input[type=hidden]").each(function() {
+//      $(this).val() //make countdown date
+// });
+
+// $(input[type=hidden]").each(function( index ) {
+//   console.log( index + ": " + $( this ).text() );
+// });
