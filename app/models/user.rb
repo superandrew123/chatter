b4780df40561a1b0489b44a1f981748c1d756527
Andrew Miksch <superandrew@Andrews-MacBook-Pro.local>
2015-04-20 09:30:20 -0400

    changed default image to bucket image

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   
   has_attached_file :avatar, 
                     :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-                    :default_url => ActionController::Base.helpers.asset_path('missing.png'),
+                    :default_url => "https://s3.amazonaws.com/flatiron-chatter/missing.png",
                     :bucket => 'flatiron-chatter'
                     
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
